077486415a62d95d7c8f6bf79087a925fc6f4c4b
gzsewell <gzsewell@gmail.com>
2025-02-05 12:38:39 -0600

    Created Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,44 @@
-import { useState } from 'react'
-import bookLogo from './assets/books.png'
+import { useState } from "react";
+import bookLogo from "./assets/books.png";
+import { Route, Routes } from "react-router-dom";
+import Account from "./components/Account";
+import Books from "./components/Books";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
-  const [token, setToken] = useState(null)
+  const [token, setToken] = useState(null);
 
   return (
     <>
-      <h1><img id='logo-image' src={bookLogo}/>Library App</h1>
+      <Routes>
+        <Route path="/account" element={<Account />} />
+        <Route path="/books" element={<Books />} />
+        <Route path="/login_page" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+      <h1>
+        <img id="logo-image" src={bookLogo} />
+        Library App
+      </h1>
 
-      <p>Complete the React components needed to allow users to browse a library catalog, check out books, review their account, and return books that they've finished reading.</p>
+      <p>
+        Complete the React components needed to allow users to browse a library
+        catalog, check out books, review their account, and return books that
+        they've finished reading.
+      </p>
 
-      <p>You may need to use the `token` in this top-level component in other components that need to know if a user has logged in or not.</p>
+      <p>
+        You may need to use the `token` in this top-level component in other
+        components that need to know if a user has logged in or not.
+      </p>
 
-      <p>Don't forget to set up React Router to navigate between the different views of your single page application!</p>
+      <p>
+        Don't forget to set up React Router to navigate between the different
+        views of your single page application!
+      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,1 +1,9 @@
 /* TODO - add your code to create a functional React component that renders a login form */
+
+import React from "react";
+
+function Login() {
+  return <div>Login</div>;
+}
+
+export default Login;
