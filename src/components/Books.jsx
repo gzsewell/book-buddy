524f0a786b2c71df7c5d03bfd4822ae4e9ac9712
gzsewell <gzsewell@gmail.com>
2025-02-05 16:02:43 -0600

    diplayed books and added some styling

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -2,10 +2,41 @@
 Fetch the book data from the provided API.
 Users should be able to click on an individual book to navigate to the SingleBook component and view its details. */
 
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Books() {
-  return <div>Books</div>;
+  const [displayedBooks, setDisplayedBooks] = useState([]);
+  useEffect(() => {
+    axios(`${import.meta.env.VITE_API_BASE_URL}books`)
+      .then((results) => {
+        setDisplayedBooks(results.data.books);
+        console.log("Book data", results.data.books);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+  return (
+    <div className="book-container">
+      {displayedBooks.map((book) => (
+        <Bookcard key={book.id} book={book} />
+      ))}
+    </div>
+  );
+}
+function Bookcard({ book }) {
+  return (
+    <div className="book-list">
+      <img
+        src={book.coverimage}
+        alt={book.title}
+        style={{ width: "200px", height: "auto" }}
+      />
+      <p>{book.title}</p>
+      <p>{book.author}</p>
+      <p>{book.description}</p>
+      <p>{book.available}</p>
+    </div>
+  );
 }
 
 export default Books;
